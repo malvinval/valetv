43a21dfe7bccd2099daf03c763717eb827159d87
malvinval <malvinval09@gmail.com>
2023-11-03 11:30:07 +0700

    update: docs

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
     return (
@@ -11,8 +11,10 @@ export default function Dashboard({ auth }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                    <div className="bg-transparent overflow-hidden sm:rounded-lg">
+                        <div className="p-6 lg:p-0 text-gray-900 text-lg">
+                            <h3 className='font-bold text-lg'>This page is still empty. Click <Link href='/tv-streams' className='text-indigo-500 underline'>here</Link> to start watching TV.</h3>
+                        </div>
                     </div>
                 </div>
             </div>
