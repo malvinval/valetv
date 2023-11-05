ba6b2ef9dad40fedaa3beab53aeca9d0b37fb887
malvinval <malvinval09@gmail.com>
2023-11-05 14:10:16 +0700

    update: welcome and dashboard content

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,19 +1,35 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, Link } from '@inertiajs/react';
+import WatchTVImg from '../../../public/watch-tv.webp';
 
 export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">{`Hello, ${auth.user.name}!`}</h2>}
+            header={<h2 className="text-xl text-gray-800 leading-tight">{`Hello, ${auth.user.name}!`}</h2>}
         >
             <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-transparent overflow-hidden sm:rounded-lg">
-                        <div className="p-6 lg:p-0 text-gray-900 text-lg">
-                            <h3 className='font-bold text-lg'>This page is still empty. Click <Link href='/tv-streams' className='text-indigo-500 underline'>here</Link> to start watching TV.</h3>
+                    <div className="px-6 bg-transparent overflow-hidden sm:rounded-lg items-center">
+                        <div className='bg-transparent flex flex-col-reverse lg:flex-row justify-between items-center mb-8'>
+                            <div id='dashboard-hero-left' className='w-full lg:w-1/2'>
+                                <div className='w-full pt-5 pb-5 lg:pt-0 md:pb-0'>
+                                    <h1 className='text-3xl md:text-4xl lg:text-5xl font-extrabold'>Welcome to ValeTV!</h1>
+                                </div>
+                                <div className='py-0 sm:py-5 w-full text-justify'>
+                                    <p className='text-xl'>ValeTV is a website application for you to watch live TV and radio broadcasts from various countries in the world for free.</p>
+                                </div>
+                                <div className='w-full flex flex-col md:flex-row py-8 md:py-0'>
+                                    <Link href='/tv-streams' className='btn btn-primary mr-0 mb-2 md:mr-2 md:mb-0'>Watch Live TV</Link>
+                                    <Link href='/radio-streams' className='btn btn-secondary text-white mr-0 mb-2 md:mr-2 md:mb-0'>Listen to Radio</Link>
+                                </div>
+                            </div>
+
+                            <div id='dashboard-hero-right' className='w-full lg:w-1/2 flex justify-start lg:justify-end'>
+                                {/* <img src={WatchTVImg} alt="" className='rounded-lg shadow-xl' /> */}
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,27 +1,51 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link, Head } from '@inertiajs/react';
+import WatchTVImg from '../../../public/watch-tv.webp';
+
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Head title="Welcome" />
             <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                {/* <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-                    
+                <div className="sm:fixed sm:top-0 sm:right-0 pt-6 text-right">
                     <Link
-                        href={route('dashboard')}
+                        href={route('login')}
                         className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                     >
-                        Dashboard
+                        Login
                     </Link>
                     
-                </div> */}
+                    <Link
+                        href={route('register')}
+                        className="px-5 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    >
+                        Register
+                    </Link>
+                </div>
 
-                <div className="max-w-7xl h-screen mx-auto p-6 lg:p-8 flex flex-col justify-center items-center">
-                    <ApplicationLogo className="fill-current" />
-                    <h1 className='text-xl md:text-2xl lg:text-4xl font-bold text-center'>Stream TV shows around the world, for free.</h1>
-                    <div className='py-5'>
-                        <Link href={route('dashboard')} className="btn btn-primary mx-auto font-bold">START WATCHING</Link>
+                <div className="pt-12">
+                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div className="px-6 bg-transparent overflow-hidden sm:rounded-lg">
+                            <div className='bg-transparent flex flex-col-reverse lg:flex-row justify-between items-center mb-8'>
+                                <div id='dashboard-hero-left' className='w-full lg:w-1/2'>
+                                    <div className='w-full pt-5 pb-5 lg:pt-0 md:pb-0'>
+                                        <h1 className='text-3xl md:text-4xl lg:text-5xl font-extrabold'>Welcome to ValeTV!</h1>
+                                    </div>
+                                    <div className='py-0 sm:py-5 w-full text-justify'>
+                                        <p className='text-xl'>ValeTV is a website application for you to watch live TV and radio broadcasts from various countries in the world for free.</p>
+                                    </div>
+                                    <div className='w-full flex flex-col md:flex-row py-8 md:py-0'>
+                                        <Link href='/tv-streams' className='btn btn-primary mr-0 mb-2 md:mr-2 md:mb-0'>Watch Live TV</Link>
+                                        <Link href='/radio-streams' className='btn btn-secondary text-white mr-0 mb-2 md:mr-2 md:mb-0'>Listen to Radio</Link>
+                                    </div>
+                                </div>
+
+                                <div id='dashboard-hero-right' className='w-full lg:w-1/2 flex justify-start lg:justify-end'>
+                                    <img src={WatchTVImg} alt="" className='rounded-lg shadow-xl' />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
